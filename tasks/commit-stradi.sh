0389ffe2ee84601dbe28e9806d6a8a31fc8e344b
Daniel Heras Quesada <dani.heras@hotmail.com>
2025-03-30 18:00:34 +0200

    feat: emoji commit message generator

---
+++ tasks/commit-stradi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+emoji="$(cat ~/.config/zed/tasks/assets/emoji_list.txt | fzf | awk -F' ' '{print $1}')"
+
+branch="$(git branch --show-current | awk -F'/' '{print $2}')"
+
+message="[$branch] $emoji "
+
+echo -n $message | pbcopy
+
+echo "$message copied to clipboard"
