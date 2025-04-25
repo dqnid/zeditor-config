90942eae3a6646773df14f5a8fa49c7f45b167d3
Daniel Heras Quesada <dani.heras@hotmail.com>
2025-04-25 09:45:23 +0200

    feat: updated config to new release + minor script improvements

--- tasks/commit-stradi.sh
+++ tasks/commit-stradi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-emoji="$(cat ~/.config/zed/tasks/assets/emoji_list.txt | fzf | awk -F' ' '{print $1}')"
+emoji="$(cat ~/.config/zed/tasks/assets/emoji_list.txt | fzf | awk -F' ' '{print $3}')"
 
 branch="$(git branch --show-current | awk -F'/' '{print $2}')"
 
